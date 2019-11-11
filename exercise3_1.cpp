07034963f2a694fc9361465feb9748686006f84c
nfpedraza <nifepegi@gmail.com>
2019-11-11 17:54:56 -0500

    recursivo_ej3

--- exercise3_1.cpp
+++ exercise3_1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 int N;
 int i;
-int fib(int N);
 int x;
 
 int fib(int N){
@@ -16,6 +15,23 @@ return fib(N-1) + fib(N-2);
 }
 }
 
+int fib2(int N, bool print = true){
+int ret=0;
+if (N>0){
+if(N<2){
+ret=1;
+}
+else{
+ret= fib2(N-1,false) + fib2(N-2,false);
+}
+if(print){
+fib2(N-1);
+std::cout<<ret<<" ";
+}
+}
+return ret;
+}
+
 int main(){
    std::cout<<"1 para recursivo o 0 para rapido  "<<"\n";
    std::cin>>x;
@@ -23,16 +39,17 @@ int main(){
    if(x==1){
     std::cout<<"ingrese numero"<<"\n";
     std::cin>>N;
-    std::cout<<"Fibonacci de "<<N<<" = "<<fib(N);
+    std::cout<<"Fibonacci de "<<N<<" = "<<fib2(N);
+    std::cout<<" ";
    }
 	
-   else if(N==0){
+   else if(x==0){
        std::cout<<"Ingrese numero"<<"\n";
        std::cin>>N;
        std::cout<<"Fast Fibo de"<<" "<<N<<"\n ";
        for(i=1;i<=N;i++){
        std::cout<<fib(i)<<" ";
-}
+ }
 }     
     else{
 	std::cout<<"mal"<<"\n";
