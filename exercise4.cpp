80efe268f69e031bb1c1289c11eb1cc5c8a5def8
nfpedraza <nifepegi@gmail.com>
2019-11-11 18:32:56 -0500

    overload_ej4

--- exercise4.cpp
+++ exercise4.cpp
@@ -4,7 +4,7 @@ int b;
 double a2;
 double b2;
 
-int getMaxInt(int a, int b){
+int getMax(int a, int b){
 if(a > b){
 std::cout<<a<<"  es mayor que  "<<b<<"\n";
 }
@@ -12,7 +12,7 @@ else{
 std::cout<<b<<"  es mayor que  "<<a<<"\n";
 }
 }
-double getMaxDouble(double a2, double b2){
+double getMax(double a2, double b2){
 if(a2>b2){
 std::cout<<a2<<" es mayor que "<<b2<<"\n";
 }
@@ -24,8 +24,8 @@ std::cout<<b2<<" es mayor que "<<a2<<"\n";
 int main(){
 std::cout<<"Ingrese los numeros a analizar"<<"\n";
 std::cout<<"numero int 1 ";std::cin>>a;std::cout<<"numero int 2 ";std::cin>>b;std::cout<<" numero dob 1 "; std::cin>>a2;std::cout<<" numero dob 2 ";std::cin>>b2;
-getMaxInt(a,b);
-getMaxDouble(a2,b2);
+getMax(a,b);
+getMax(a2,b2);
 return 0;
 }
  
