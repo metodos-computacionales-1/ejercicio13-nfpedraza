59b160697daaf2e4f8867b6b5e88f875b6b2daa3
nfpedraza <nifepegi@gmail.com>
2019-11-11 23:29:36 -0500

    cambios_flag

--- exercise5_2.cpp
+++ exercise5_2.cpp
@@ -4,6 +4,8 @@
 const int VAR 4;
 int main(void)
 {
+    std::cout.precision(16);
+    std::cout.setf(std::ios::scientific);
     std::cout<<std::sin(VAR)<<std::endl;
     std::cout<<M_PI<<std::endl;
     std::cout<<std::cout(M_PI)<<std::endl;
