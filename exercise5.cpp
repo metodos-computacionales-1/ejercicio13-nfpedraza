ff1bb16ac423e23077ae117857deb41d50e67f94
nfpedraza <nifepegi@gmail.com>
2019-11-11 23:25:42 -0500

    last_flag

--- exercise5.cpp
+++ exercise5.cpp
@@ -1,10 +1,12 @@
 
 // my_program.cpp
 #include <iostream>
+#include <cmath>
+
 const int VAR 4;
 
 int main(void)
 {
-std::cout<<VAR<<std::endl; 
+std::cout<<std::sin(VAR)<<std::endl; 
    return 0;
 }
