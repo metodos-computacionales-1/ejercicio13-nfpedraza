58651f45ba182c4be95395b0d09ac4ae28dda54f
nfpedraza <nifepegi@gmail.com>
2019-11-11 23:27:53 -0500

    mas_flag

---
+++ exercise5_2.cpp
@@ -0,0 +1,11 @@
+// my_program.cpp
+#include <iostream>
+#include <cmath>
+const int VAR 4;
+int main(void)
+{
+    std::cout<<std::sin(VAR)<<std::endl;
+    std::cout<<M_PI<<std::endl;
+    std::cout<<std::cout(M_PI)<<std::endl;
+    return 0;
+}
